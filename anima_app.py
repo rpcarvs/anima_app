3d0adcd38d68182c5c87069d85d64d960d6a7330
Rodrigo Carvalho <rodrigo.carvalho.al@gmail.com>
2025-04-17 18:29:18 +0200

    cosmetics

--- anima_app.py
+++ anima_app.py
@@ -158,7 +158,7 @@ with st.container(border=False):
 
         with st.container(border=True):
             st.write(f"**Molecule after preparation:** {new_molecule}")
-            st.write(f"**Insertion potential:** $${{\color{{green}}{round(float(li_voltage), 3)}}} \\textrm{{ V v.s. Li/Li}}^+$$")
+            st.write(f"**Lithium insertion potential:** $${{\color{{green}}{round(float(li_voltage), 3)}}} \\textrm{{ V v.s. Li/Li}}^+$$")
             st.write(f"**Oxidation potential:** $${{\color{{red}}{round(float(oxidation), 3)}}} \\textrm{{ V (ref to vacuum)}}$$")
             st.write(f"**Reduction potential:** $${{\color{{blue}}{round(float(reduction), 3)}}} \\textrm{{ V (ref to vacuum)}}$$")
 
