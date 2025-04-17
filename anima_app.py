ea66c9c9e4db162065ef91fdb4e787de5abee314
Rodrigo Carvalho <rodrigo.carvalho.al@gmail.com>
2025-04-17 18:27:56 +0200

    cosmetics

--- anima_app.py
+++ anima_app.py
@@ -158,9 +158,9 @@ with st.container(border=False):
 
         with st.container(border=True):
             st.write(f"**Molecule after preparation:** {new_molecule}")
-            st.write(f"**Insertion potential:** $${{\color{{green}}{round(li_voltage, 2)}}} \\textrm{{ V v.s. Li/Li}}^+$$")
-            st.write(f"**Oxidation potential:** $${{\color{{red}}{round(oxidation, 2)}}} \\textrm{{ V (ref to vacuum)}}^+$$")
-            st.write(f"**Reduction potential:** $${{\color{{blue}}{round(reduction, 2)}}} \\textrm{{ V (ref to vacuum)}}^+$$")
+            st.write(f"**Insertion potential:** $${{\color{{green}}{round(float(li_voltage), 2)}}} \\textrm{{ V v.s. Li/Li}}^+$$")
+            st.write(f"**Oxidation potential:** $${{\color{{red}}{round(float(oxidation), 2)}}} \\textrm{{ V (ref to vacuum)}}$$")
+            st.write(f"**Reduction potential:** $${{\color{{blue}}{round(float(reduction), 2)}}} \\textrm{{ V (ref to vacuum)}}$$")
 
             m = Chem.MolFromSmiles(new_molecule)
             fig = Draw.MolToImage(m)
